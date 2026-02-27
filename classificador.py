0b029841cca3105d0205072c0e8ffa6af9e3ef71
gustaxxcwb <gustavo.schuarca@escola.pr.gov.br>
2026-02-27 11:16:33 -0300

    Create classificador.py

---
+++ classificador.py
@@ -0,0 +1,15 @@
+# Solicita um número ao usuário
+numero = float(input("__________________________"))
+
+# Verifica se o número é positivo
+if numero > 0:
+    # Se for positivo, imprime a mensagem
+    print("O número é _________________.")
+# Verifica se o número é negativo
+elif numero < 0:
+    # Se for negativo, imprime a mensagem
+    print("_______________________________________")
+# Se não for positivo nem negativo, é zero
+else:
+    # Imprime a mensagem para zero
+    print("__________________________________")
